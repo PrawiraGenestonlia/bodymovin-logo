14007e68b81e3e24c5c0a8986df9aab312e5cf8b
Prawira GENESTONLIA <prawira@gmail.com>
2020-08-30 15:49:15 +0800

    Updates

---
+++ precache-manifest.3387750c8f7b9ca26c79d846c4e3abbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed87f08ab30700873d8d63a9f46ead",
+    "url": "/bodymovin-logo/index.html"
+  },
+  {
+    "revision": "635f32f17c6533dea06e",
+    "url": "/bodymovin-logo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73af812e2d51c6269c6e",
+    "url": "/bodymovin-logo/static/js/2.443a7bc8.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/bodymovin-logo/static/js/2.443a7bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635f32f17c6533dea06e",
+    "url": "/bodymovin-logo/static/js/main.df8afb1d.chunk.js"
+  },
+  {
+    "revision": "0c7a9010b0847daaa7fc",
+    "url": "/bodymovin-logo/static/js/runtime-main.5d66d329.js"
+  }
+]);
