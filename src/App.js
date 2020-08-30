fddc884f98448050c0dc3701b711dcda3183e0e3
Prawira GENESTONLIA <prawira@gmail.com>
2020-08-30 15:51:42 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import lottie from 'lottie-web';
+import { useMouseState, useWindowResize } from 'beautiful-react-hooks';
+
+const TOTAL_FRAME = 150;
 
 function App() {
+  const ref = useRef();
+  const { clientX, clientY } = useMouseState(ref);
+  const [animation, setAnimation] = useState('');
+  const [width, setWidth] = useState(window.innerWidth);
+  // const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: document.getElementById('bm'), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      path: 'T&O Logo.json' // the path to the animation json
+    });
+    setAnimation(anim);
+  }, []);
+
+  useWindowResize((event) => {
+    setWidth(window.innerWidth);
+    // setHeight(window.innerHeight);
+  });
+
+  useEffect(() => {
+    const frame = parseInt(Number(clientX) * TOTAL_FRAME / Number(width));
+    // console.log(frame);
+    animation && animation.goToAndStop(frame, 1);
+  }, [animation, clientX, clientY, width]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="bm" style={{ width: '1080px', height: '100%' }} ref={ref}>
+
     </div>
   );
 }
